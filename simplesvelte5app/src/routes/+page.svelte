6a3f3a95db220b349701082b1bab3fa27c9b63e8
MaxMph <hurleymax255@gmail.com>
2026-03-15 21:39:15 -0400

    Biiiiiiiiiiiig update

--- simplesvelte5app/src/routes/+page.svelte
+++ simplesvelte5app/src/routes/+page.svelte
@@ -1,2 +1,141 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="bg">
+    <div class="main_div">
+        <div class="intro">
+            <div class="img_holder">
+                <img src="src/lib/assets/sd card 1 (indexed).gif" alt="">
+                <img src="src/lib/assets/header.png" alt="">
+                <img src="src/lib/assets/sd card 1 (indexed).gif" alt="">
+                <!-- <h1>!Wecome to Compact!</h1> -->
+            </div>
+        </div>
+        <div class="basic_box">
+            <p>Guy I really want to make videogames, but I barely have any space on my computer!!! I guess I just won't...</p>
+            <img src="src/lib/assets/shrug.png" alt="">
+            <p> but wait... mario 64 only takes up 8mb! If they can do all THAT, I cant just give up that easily! I suppose I could try and make something with my last 64mb!</p>
+            
+            <p>That shoulnt be too hard, thats 8 TIMES the size of mario 64...</p>
+            <img src="src/lib/assets/mario 64.png" alt="">
+            <!-- <p> &#60;('.'&#60;) &#60;('.')&#62; (&#62;'.')&#62; </p> -->
+            <p>&#60;:::::::::::::&#93;==()&#60;('.')&#62;()==&#91;:::::::::::::&#62;</p>
+            <!-- <p>_//  \\_</p> -->
+
+            <div style="outline: 2px dashed white; padding: 16px;">
+                <h1>YOU SHIP:</h1>
+                <h2> A game &#60;= 64MB </h2>
+                <!-- <p>&#60;:::::::::::::&#93;==()&#60;('.')&#62;()==&#91;:::::::::::::&#62;</p> -->
+               <p>--------------------------------------------------------</p>
+                <h1>WE SHIP:  </h1>
+                <h2>A thumbdrive with all the shipped games + storage and computer parts</h2>
+            </div>
+
+            <p>&#60;:::::::::::::&#93;==()&#60;('.')&#62;()==&#91;:::::::::::::&#62;</p>
+            <img src="src/lib/assets/sd card 1 (indexed).gif" alt="">
+            <p>_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~._</p>
+            <!-- <p>(-(-_(-_-)_-)-)</p> -->
+             <h1>!PRIZES!</h1>
+            <p>_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~._</p>
+
+            <div style="outline: 2px dashed white; padding: 16px; width: 256px; margin: auto;">
+            <br>
+                <!-- <img src="src/lib/assets/rewards/ereader.png" alt="">
+                <p>Xteink X4 <br> -(15 Hours)- </p> -->
+                
+                <img src="src/lib/assets/rewards/thumbdrive.png" alt="">
+                <p>64gb/128gb thumbdrive <br> -(4/6 Hours)-</p>
+                <!-- <p>64gb thumbdrive <br> -(4 Hours)- <br> 128gb thumbdrive <br> -(6 Hours)- </p> -->
+
+                <img src="src/lib/assets/rewards/ssd.png" alt="">
+                <p>$50 storage grant <br> -(10 Hours)- </p>
+
+                <img src="src/lib/assets/rewards/handheld.png" alt="">
+                <p>Emulation Handheld <br> -(15 Hours)- </p>
+
+
+            </div>
+            <br>
+            <p>_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~._</p>
+            <!-- <p>(-(-_(-_-)_-)-)</p> -->
+             <h1>!PRIZES!</h1>
+            <p>_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~._</p>
+            <img src="src/lib/assets/sd card 1 (indexed).gif" alt="">
+            <br>
+            <br>
+            
+            <p>_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~._</p>
+            <div class="img_holder">
+            <img src="src/lib/assets/rewards/money.png" alt="">
+            <button style="width: 128px; height: 32px; ">$submit$</button>
+            <img src="src/lib/assets/rewards/money.png" alt="">
+            </div>
+            <p>_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~._</p>
+
+        </div>
+    </div>
+</div>
+
+<style>
+    :global(body) {
+    margin: 0;
+    }
+
+    p {
+        font-family:'Courier New', Courier, monospace;
+    }
+
+    .bg {
+        background-color: black;
+        background-image: url(src/lib/assets/pbc bg.png);
+        margin: 0%;
+    }
+
+    .main_div {
+        height: 2560px;
+        /* width: 528px; */
+        /* width: fit-content; */
+        max-width: 800px;
+        background-color: rgba(0, 0, 0, 0.658);
+        /* background-color: #507c38; */
+        margin: auto;
+        display: grid;
+        justify-content: center;
+        align-content: start;
+    }
+
+    .intro {
+        /* align-self: center; */
+        /* display: flexbfeox;
+        justify-self: center; */
+        margin-top: 0px;
+        padding: 16px;
+        text-align: center;
+        /* width: fit-content; */
+        height: fit-content;
+        
+        /* margin: 0px; */
+        /* width: 100%; */
+        /* width: fit-content; */
+        /* justify-self: start; */
+        /* display: flexbox; */
+        background-color: rgba(252, 75, 255, 0.159);
+    }
+
+    .basic_box {
+        /* background-color: rgba(255, 58, 206, 0.425); */
+        /* color: #b8fcb8; */
+        color: white;
+        margin-top: 0px;
+        padding: 32px;
+        height: fit-content;
+        max-width: 100%;
+        text-align: center;
+    }
+
+    .img_holder {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 16px;
+    }
+
+</style>
